9da4bf8192bc00ac0a9d8ae709456d79329335aa
Renan de Castro Rodrigues <89025568+renan-castro@users.noreply.github.com>
2022-06-06 21:21:12 -0300

    Add files via upload

---
+++ javafx/Visual/Home.java
@@ -0,0 +1,25 @@
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+
+public class Primeiratela {
+  
+    
+
+    @FXML
+    private Menu menuApoiador;
+
+    @FXML
+    private Menu menuColeta;
+
+    @FXML
+    private Menu menuInicio;
+
+    @FXML
+    private Menu menuQuemsomos;
+
+    @FXML
+    private MenuBar menubar;
+
+}
---
+++ javafx/Visual/QuemSomos.java
@@ -0,0 +1,15 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Quartatela {
+
+    @FXML
+    private Button btnVoltar3;
+
+    @FXML
+    void acaoVoltar3(ActionEvent event) {
+
+    }
+
+}
