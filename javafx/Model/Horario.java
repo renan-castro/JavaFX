97153eb1f9f9a0315272a6d62ed39703cc508dfb
Renan de Castro Rodrigues <89025568+renan-castro@users.noreply.github.com>
2022-06-17 18:46:53 -0300

    Add files via upload

--- javafx/Model/Horario.java
+++ javafx/Model/Horario.java
@@ -1,35 +1,56 @@
-package modelo;
-
-import javafx.beans.property.SimpleIntegerProperty;
+package Model;
 import javafx.beans.property.SimpleStringProperty;
 
-public class Horario {
-    private SimpleIntegerProperty idade;
-    private SimpleStringProperty nome;
-    private SimpleStringProperty email;
+public class Horario{
+    private SimpleStringProperty segunda = new SimpleStringProperty();
+    private SimpleStringProperty terca = new SimpleStringProperty();
+    private SimpleStringProperty quarta = new SimpleStringProperty();
+    private SimpleStringProperty quinta = new SimpleStringProperty();
+    private SimpleStringProperty sexta = new SimpleStringProperty();
+    private SimpleStringProperty sabado = new SimpleStringProperty();
 
-    public Horario(int idade, String nome, String email) {
-        this.idade = new SimpleIntegerProperty(idade);
-        this.nome = new SimpleStringProperty(nome);
-        this.email= new SimpleStringProperty(email);
+    public Horario(String segunda, String terca, String quarta, String quinta, String sexta, String sabado){
+        this.segunda.set(segunda);
+        this.terca.set(terca);
+        this.quarta.set(quarta);
+        this.quinta.set(quinta);
+        this.sexta.set(sexta);
+        this.sabado.set(sabado);
     }
-
-    public int getIdade(){
-        return idade.get();
+    public String getSegunda(){
+        return segunda.get();
     }
-    public void setIdade(int idade){
-        this.idade.set(idade);
+    public void setSegunda(String segunda){
+        this.segunda.set(segunda);
     }
-    public String getNome(){
-        return nome.get();
+    public String getTerca(){
+        return terca.get();
     }
-    public void setNome(String nome){
-        this.nome.set(nome);
+    public void setTerca(String terca){
+        this.terca.set(terca);
     }
-    public String getEmail(){
-        return email.get();
+    public String getQuarta(){
+        return quarta.get();
     }
-    public void setEmail(String email){
-        this.email.set(email);
+    public void setQuarta(String quarta){
+        this.quarta.set(quarta);
+    }
+    public String getQuinta(){
+        return quinta.get();
+    }
+    public void setQuinta(String quinta){
+        this.quinta.set(quinta);
+    }
+    public String getSexta(){
+        return sexta.get();
+    }
+    public void setSexta(String sexta){
+        this.sexta.set(sexta);
+    }
+    public String getSabado(){
+        return sabado.get();
+    }
+    public void setSabado(String sabado){
+        this.sabado.set(sabado);
     }
 }
